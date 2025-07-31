f06179e3f4d3412676c6f44b4582ffcf891a2875
Blu Corbel <blucorbel29@gmail.com>
2025-07-31 23:16:44 +0100

    Update ports_randomizer.py

--- ports/ports_randomizer.py
+++ ports/ports_randomizer.py
@@ -1,1 +1,13 @@
+import random
+import socket
 
+def get_random_open_port():
+    for _ in range(100):
+        port = random.randint(1024, 65535)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(("0.0.0.0", port))
+                return port
+            except OSError:
+                continue
+    raise Exception("No free port found")
